366b0f5de86f8310c51505e032f1bbc0800cb3c0
nimanima734 <nyma90610@gmail.com>
2025-05-23 11:56:28 +0100

    Update ip_restrict_middleware.py

--- Gestion_Pharma/ip_restrict_middleware.py
+++ Gestion_Pharma/ip_restrict_middleware.py
@@ -1,1 +1,28 @@
+# ip_restrict_middleware.py
+from django.http import HttpResponseForbidden
+
+# هنا تحط قائمة عناوين الـ IP المسموح بها
+ALLOWED_IPS = [
+    '192.168.8.114',   # مثال IP داخلي
+    '154.255.40.171',   # مثال IP خارجي
+]
+
+class RestrictIPMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        ip = self.get_client_ip(request)
+        if ip not in ALLOWED_IPS:
+            return HttpResponseForbidden("<h1>403 - Accès refusé</h1>")
+        return self.get_response(request)
+
+    def get_client_ip(self, request):
+        """تحصل على عنوان IP الحقيقي حتى لو كان فيه proxy"""
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0].strip()
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
 
