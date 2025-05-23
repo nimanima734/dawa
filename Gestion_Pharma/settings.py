3777768a059272a582a6284726f6161e7762dca8
SraouiBouchra123 <bouchrasraoui09@gmail.com>
2025-05-24 00:16:21 +0100

    Update settings.py

--- Gestion_Pharma/settings.py
+++ Gestion_Pharma/settings.py
@@ -50,7 +50,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'dawapharma91@gmail.com'
-EMAIL_HOST_PASSWORD = 'awma bqdh xrzx gwsc'
+EMAIL_HOST_PASSWORD = 'awmabqdhxrzxgwsc'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
