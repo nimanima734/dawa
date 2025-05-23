f4b86a5442a03dac06406a4e9f4bd8e4cf2f6836
nimanima734 <nyma90610@gmail.com>
2025-05-23 23:49:16 +0100

    Update views.py

--- Utilisateurs/views.py
+++ Utilisateurs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import user_passes_test
 
 from django.contrib.auth import login,authenticate, logout
 from django.contrib import messages
