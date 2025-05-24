14e3cefcb37db82e0bb442df42f294042eedf083
SraouiBouchra123 <bouchrasraoui09@gmail.com>
2025-05-24 20:43:25 +0100

    Update views.py

--- Utilisateurs/views.py
+++ Utilisateurs/views.py
@@ -1,4 +1,4 @@
-rom django.shortcuts import render, redirect
+from django.shortcuts import render, redirect
 
 from django.contrib.auth import login,authenticate, logout
 from django.contrib import messages
