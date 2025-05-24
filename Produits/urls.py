f1b18c9afac2521029452084ecd2b35dab18c884
SraouiBouchra123 <bouchrasraoui09@gmail.com>
2025-05-24 18:23:57 +0100

    Update urls.py

--- Produits/urls.py
+++ Produits/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from .views import * 
 from django.conf import settings
 from django.conf.urls.static import static
-from Utilisateurs.views import * 
 from django.views.generic import TemplateView
-
 
 urlpatterns = [
  
